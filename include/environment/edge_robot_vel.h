493f6f01267544e83346355e72dac081d43d1a5e
Luca Rossini <luca.rossini@iit.it>
2021-05-28 18:14:07 +0200

    edge_robot_vel split in source and header file + added unary velocity limits edge for single configuration optimization

--- include/environment/edge_robot_vel.h
+++ include/environment/edge_robot_vel.h
@@ -2,6 +2,7 @@
 #define EDGE_ROBOT_VEL_H
 
 #include <g2o/core/base_binary_edge.h>
+#include <g2o/core/base_unary_edge.h>
 
 #include <XBotInterface/ModelInterface.h>
 
@@ -17,47 +18,28 @@
      EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
      typedef BaseBinaryEdge<-1, Eigen::VectorXd, VertexRobotPos, VertexRobotPos> BaseEdge;
 
-     EdgeRobotVel(XBot::ModelInterface::Ptr model) :
-     BaseBinaryEdge<-1, Eigen::VectorXd, VertexRobotPos, VertexRobotPos>(),
-     _model(model)
-     {
-         _model->getVelocityLimits(_vel_max);
-         _vel_max = _vel_max / 100;
-         _vel_min = -_vel_max;
-     }
+     EdgeRobotVel(XBot::ModelInterface::Ptr model);
 
      bool read(std::istream& is)
-     {}
+     {
+         Eigen::VectorXd meas;
+         internal::readVector(is, meas);
+         setMeasurement(meas);
+
+         return is.good() || is.eof();
+     }
 
      bool write(std::ostream& os) const
-     {}
-
-     void computeError()
      {
-         auto v1 = dynamic_cast<const VertexRobotPos*>(BaseEdge::_vertices[0]);
-         auto v2 = dynamic_cast<const VertexRobotPos*>(BaseEdge::_vertices[1]);
+         Eigen::VectorXd p = measurement();
+         os << p;
 
-         Eigen::VectorXd q1(v1->estimateDimension()), q2(v1->estimateDimension()), diff(v1->estimateDimension());
-         q1 = v1->estimate();
-         q2 = v2->estimate();
-
-         double eps = 0.1;
-         double S = 0.05;
-         double r = 0.2;
-         int n = 2;
-         diff = (q2 - q1)/0.01;
-
-         for (int i = 0; i < q1.size(); i++)
-         {
-             if (diff(i) > _vel_max(i) + eps)
-             {
-                double value = pow((-diff(i)-(-_vel_max(i)-eps))/S, n);
-                _error(i) = value;
-             }
-             else
-                 _error(i) = 0;
-         }
+         return os.good();
      }
+
+     void resize();
+
+     void computeError();
 
  private:
      XBot::ModelInterface::Ptr _model;
@@ -66,6 +48,47 @@
 
 
 
- }; } }
+ };
+
+ class EdgeRobotUnaryVel : public BaseUnaryEdge<-1, Eigen::VectorXd, VertexRobotPos> {
+ public:
+     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+
+     EdgeRobotUnaryVel(XBot::ModelInterface::Ptr model);
+
+
+     bool read(std::istream& is)
+     {
+         Eigen::VectorXd meas;
+         internal::readVector(is, meas);
+         setMeasurement(meas);
+
+         return is.good() || is.eof();
+     }
+
+     bool write(std::ostream& os) const
+     {
+         Eigen::VectorXd p = measurement();
+         os << p;
+
+         return os.good();
+     }
+
+     void resize();
+
+     void setRef(Eigen::VectorXd ref);
+
+     void getRef(Eigen::VectorXd ref) const;
+
+     void computeError();
+
+
+ private:
+     XBot::ModelInterface::Ptr _model;
+
+     Eigen::VectorXd _ref;
+ };
+
+ } }
 
 #endif
---
+++ src/environment/edge_robot_vel.cpp
@@ -0,0 +1,136 @@
+#include <environment/edge_robot_vel.h>
+
+using namespace XBot::HyperGraph;
+using namespace g2o;
+
+EdgeRobotVel::EdgeRobotVel(XBot::ModelInterface::Ptr model):
+BaseBinaryEdge<-1, Eigen::VectorXd, VertexRobotPos, VertexRobotPos>(),
+_model(model)
+{
+    _model->getVelocityLimits(_vel_max);
+    _vel_max = _vel_max;
+    _vel_min = -_vel_max;
+}
+
+void EdgeRobotVel::resize()
+{
+    auto v1 = dynamic_cast<const VertexRobotPos*>(_vertices[0]);
+
+    if (v1 == nullptr)
+        std::runtime_error("first set vertices, then resize!");
+
+    _error.conservativeResize(v1->estimateDimension());
+    _measurement.conservativeResize(v1->estimateDimension());
+}
+
+void EdgeRobotVel::computeError()
+{
+    auto v1 = dynamic_cast<const VertexRobotPos*>(_vertices[0]);
+    auto v2 = dynamic_cast<const VertexRobotPos*>(_vertices[1]);
+
+    Eigen::VectorXd q1(v1->estimateDimension()), q2(v1->estimateDimension()), diff(v1->estimateDimension());
+    q1 = v1->estimate();
+    q2 = v2->estimate();
+   _error.setZero(v1->estimateDimension());
+
+    double eps = 0.005;
+    double S = 0.05;
+    double r = 0.2;
+    int n = 2;
+    diff = (q2/0.01 - q1/0.01);
+
+    S = 3;
+    for (int i = 0; i < q1.size(); i++)
+    {
+        double value = 1/exp(S*diff(i) - _vel_min(i)) + exp(S*diff(i) - _vel_max(i));
+        _error(i) = value;
+    }
+//    for (int i = 0; i < q1.size(); i++)
+//    {
+//        if (diff(i) > _vel_max(i) - eps)
+//        {
+//            double value = pow((-diff(i)-(-_vel_max(i)-eps))/S, n);
+//            _error(i) = value;
+//        }
+//        else
+//            _error(i) = 0;
+//    }
+
+//    for (int i = 0; i < q1.size(); i++)
+//    {
+//        if (diff(i) < _vel_min(i) + eps)
+//        {
+//           double value = pow((-diff(i)-(-_vel_min(i)-eps))/S, n);
+//           _error(i) += value;
+//        }
+//        else
+//            _error(i) += 0;
+//    }
+}
+
+EdgeRobotUnaryVel::EdgeRobotUnaryVel(XBot::ModelInterface::Ptr model):
+BaseUnaryEdge<-1, Eigen::VectorXd, VertexRobotPos>(),
+_model(model)
+{}
+
+void EdgeRobotUnaryVel::resize()
+{
+    auto v1 = dynamic_cast<const VertexRobotPos*>(_vertices[0]);
+
+    if (v1 == nullptr)
+        std::runtime_error("first set vertices, then resize!");
+
+    _error.conservativeResize(v1->estimateDimension());
+    _measurement.conservativeResize(v1->estimateDimension());
+}
+
+void EdgeRobotUnaryVel::setRef(Eigen::VectorXd ref)
+{
+    _ref = ref;
+}
+
+void EdgeRobotUnaryVel::getRef(Eigen::VectorXd ref) const
+{
+    ref = _ref;
+}
+
+void EdgeRobotUnaryVel::computeError()
+{
+    auto v1 = dynamic_cast<const VertexRobotPos*>(_vertices[0]);
+
+    Eigen::VectorXd q1(v1->estimateDimension()), diff(v1->estimateDimension());
+    Eigen::VectorXd vel_max, vel_min;
+    _model->getVelocityLimits(vel_max);
+    vel_min = -vel_max;
+    q1 = v1->estimate();
+   _error.setZero(v1->estimateDimension());
+
+    double eps = 0.005;
+    double S = 0.05;
+    double r = 0.2;
+    int n = 2;
+    diff = (_ref/0.01 - q1/0.01);
+
+    for (int i = 0; i < q1.size(); i++)
+    {
+        if (diff(i) > vel_max(i) - eps)
+        {
+            double value = pow((-diff(i)-(-vel_max(i)-eps))/S, n);
+            _error(i) = value;
+        }
+        else
+            _error(i) = 0;
+    }
+
+    for (int i = 0; i < q1.size(); i++)
+    {
+        if (diff(i) < vel_min(i) + eps)
+        {
+           double value = pow((-diff(i)-(-vel_min(i)-eps))/S, n);
+           _error(i) += value;
+        }
+        else
+            _error(i) += 0;
+    }
+}
+
