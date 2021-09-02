fc7cc893d489a17b278ed5bc6424c8425f697a48
Luca Rossini <luca.rossini@iit.it>
2021-09-02 13:59:41 +0200

    added zedm files

--- src/nodes/vision_node.cpp
+++ src/nodes/vision_node.cpp
@@ -5,12 +5,22 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "visual_node");
     ros::NodeHandle nh("");
 
-    XBot::HyperGraph::Utils::PointCloudManager pc_manager(Eigen::Vector3d(0.0, 0.0, 0.0), nh);
+    XBot::HyperGraph::Utils::PointCloudManager::Ptr pc_manager;
+
+    if (argc == 2)
+    {
+        std::string topic_name(argv[1]);
+        pc_manager = std::make_shared<XBot::HyperGraph::Utils::PointCloudManager>(topic_name, nh);
+    }
+    else
+    {
+        pc_manager = std::make_shared<XBot::HyperGraph::Utils::PointCloudManager>(Eigen::Vector3d(0.0, 0.0, 0.0), nh);
+    }
 
     ros::Rate rate(30);
     while (ros::ok())
     {
-        pc_manager.run();
+        pc_manager->run();
 
         rate.sleep();
         ros::spinOnce();
