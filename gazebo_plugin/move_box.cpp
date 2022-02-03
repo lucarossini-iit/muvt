e9b55ba1b6507e637a72be01c1388c1cc3889764
Luca Rossini <luca.rossini@iit.it>
2022-02-03 13:05:16 +0100

    trajectory fix

--- gazebo_plugin/move_box.cpp
+++ gazebo_plugin/move_box.cpp
@@ -20,7 +20,7 @@ namespace gazebo
               // name the animation "test",
               // make it last 10 seconds,
               // and set it on a repeat loop
-              new gazebo::common::PoseAnimation("test", 10.0, true));
+              new gazebo::common::PoseAnimation("test", 13.0, true));
 
         gazebo::common::PoseKeyFrame *key;
 
@@ -30,22 +30,22 @@ namespace gazebo
         key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
         // set first waypoint
-        key = anim->CreateKeyFrame(3.0);
-        key->Translation(ignition::math::Vector3d(0.976034, 0.3, 1.22682));
+        key = anim->CreateKeyFrame(5.0);
+        key->Translation(ignition::math::Vector3d(0.976034, 0.2, 1.22682));
         key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
         // set second waypoint
-        key = anim->CreateKeyFrame(5.0);
+        key = anim->CreateKeyFrame(7.0);
         key->Translation(ignition::math::Vector3d(0.976034, 0.1, 1.22682));
         key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
         // set third waypoint
-        key = anim->CreateKeyFrame(7.0);
+        key = anim->CreateKeyFrame(10.0);
         key->Translation(ignition::math::Vector3d(0.946034, 0.1, 1.42682));
         key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
         // move back to starting point
-        key = anim->CreateKeyFrame(10.0);
+        key = anim->CreateKeyFrame(13.0);
         key->Translation(ignition::math::Vector3d(0.976034, -0.1, 1.22682));
         key->Rotation(ignition::math::Quaterniond(0, 0, 0));
 
